95317f3cec3a07ae64ac4a2e6a05c23b5d765e55
John Farrell <jsfarrell12@gmail.com>
2023-01-28 20:33:32 -0500

    getRecent

--- Controllers/DBControllers.js
+++ Controllers/DBControllers.js
@@ -75,6 +75,8 @@ const getRecentStory = async (req, res) => {
     .db("stories")
     .collection("stories")
     .find()
+    .sort({ date: -1 })
+    .limit(10)
     .then((info) => {
       res.status(200).json({ result: info });
     })
